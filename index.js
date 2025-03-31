6197aa9fddb8b359d33e86fe4e81054fcaaeedc7
Eugene Kapombe <kapombek@gmail.com>
2025-03-31 14:47:17 +0300

    added selector function

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file!
+const scuberDrivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+const returnFirstTwoDrivers = (scuberDrivers) => {
+    return scuberDrivers.slice(0, 2);
+};
+
+const returnLastTwoDrivers = () => { 
+    return scuberDrivers.slice(-2);
+};
+
+const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers];
+
+const createFareMultiplier=(multiplier)=>{
+    return (fare) => {
+        return fare * multiplier;
+        };
+};
+     
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+
+const selectDifferentDrivers = (scuberDrivers,selectDriver) => { 
+    return selectDriver(scuberDrivers);
+};
+
